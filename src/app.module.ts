0fbe84652c4eb691dad7dfcfc1598cf9d744c362
CoviloMilos <miloscovilons@gmail.com>
2022-09-02 16:05:39 +0200

    Health module included in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './api/health/health.module';
 import { UserModule } from './api/user/user.module';
 
 import { envConfig } from './config';
 
-const MODULES = [ConfigModule.forRoot(envConfig()), UserModule];
+const MODULES = [ConfigModule.forRoot(envConfig()), HealthModule, UserModule];
 
 @Module({
   imports: [...MODULES],
